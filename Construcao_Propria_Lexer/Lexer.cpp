4ca74cb47312e5f85b0e65996d7cc0fb6f98d38b
RafaelSantosBraz <rafinha.santos.braz@gmail.com>
2018-04-06 08:08:54 -0300

    Implementação básica de um Lexer

--- Construcao_Propria_Lexer/Lexer.cpp
+++ Construcao_Propria_Lexer/Lexer.cpp
@@ -1,18 +1,137 @@
+#include <cstdio>
+#include <iostream>
+#include <cctype>
+#include <cstdlib>
+
+#define NUM 256
+#define SUB 257
+#define SOM 258
 
 using namespace std;
 
-struct TRANSICAO{
-    int atual;
-    char simbolo;
-    int prox;
+struct TOKEN
+{
+    int id;
+    char valor;
 };
+
+void list();
+void term();
+void x();
+void match(char);
+void print(char);
+
+TOKEN lookahead;
+string input;
+int pos = 0;
+
+void print(char c)
+{
+    printf("%c", c);
+}
+
+TOKEN nextToken()
+{
+    TOKEN token;
+    if (pos < input.length())
+    {
+        if (isdigit(input[pos]))
+        {
+            token.id = NUM;
+            token.valor = input[pos++];
+        }
+        else if (input[pos] == '+')
+        {
+            token.id = SOM;
+            token.valor = input[pos++];
+        }
+        else if (input[pos] == '-')
+        {
+            token.id = SUB;
+            token.valor = input[pos++];
+        }
+        else
+        {
+            token.id = EOF;
+        }
+        return token;
+    }
+    else
+    {
+        token.id = EOF;
+        return token;
+    }
+}
+
+void list()
+{
+    printf("list\n");
+    term();
+    x();
+}
+
+void x()
+{
+    //printf("x\n");
+    if (lookahead.valor == '+')
+    {
+        match('+');
+        term();
+        print('+');
+        x();
+    }
+    else if (lookahead.valor == '-')
+    {
+        match('-');
+        term();
+        print('-');
+        x();
+    }
+    else if (lookahead.id == EOF)
+    {
+        printf("\nSuccess.\n");
+    }
+    else
+    {
+        printf("Syntax error on x\n");
+        exit(1);
+    }
+}
+
+void term()
+{
+    //printf("term\n");
+    // if (isalpha(lookahead)){ //var
+    //     match(lookahead);
+    // } else
+    if (isdigit(lookahead.valor))
+    {
+        print(lookahead.valor);
+        match(lookahead.valor);
+    }
+    else
+    {
+        printf("Syntax error on term\n");
+        exit(1);
+    }
+}
+
+void match(char s)
+{
+    //printf("match %c\n", s);
+    if (lookahead.valor == s)
+    {
+        lookahead = nextToken();
+    }
+    else
+    {
+        printf("Match error");
+    }
+}
 
 int main()
 {
-    TRANSICAO numero[2];
-    numero[0].atual = 0;
-    numero[0].prox = 1;
-    numero[0].simbolo = '0';
-    
-    return 0;
+    input = "2+2-2";
+    lookahead = nextToken();
+    list();
 }
